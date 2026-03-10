f1a481b35b73c2cf80d199225dbc534534a0c273
Administrator <Administrator@A316-61>
2026-03-10 14:47:08 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 10;
+                int b = 100;
+                int c;
+
+                if ( b > 10 )
+                {
+                    throw new AbandonedMutexException("值大於 10");
+                }
+
+                c = a / b;
+
+                string[] name = new string[5];
+                string id = name[4];
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("ERROR: " + ex.ToString());
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("ERROR: " + ex.ToString());
+            }
+            catch (AbandonedMutexException ex)
+            {
+                Console.WriteLine("ERROR: " + ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("END");
+            }
+        }
+    }
+}
